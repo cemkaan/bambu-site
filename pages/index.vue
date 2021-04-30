57ebd5d208f49f238c675fbe33b15b10ce6cd477
CEM KAAN KÖSALI <cemkaan@inbox.ru>
2021-04-30 17:34:09 +0300

    bismillah

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="col-md-8 py-5 px-3 mx-auto">
+    <header class="pb-3 mb-5 border-bottom">
+      <h1 class="h4">
+        <a
+          href="/"
+          class="d-flex align-items-center text-dark text-decoration-none"
+        >
+          <img height="250" src="~/assets/img/bambu.png" alt="bambu" />
+        </a>
+      </h1>
+    </header>
+
+    <h1>RePlay</h1>
+    <div class="col-lg-8 px-0">
+      <p class="lead">
+        Oyunda amacımız kullanıcının hareket etmesi. Oyunda kayıt edilmiş konuma
+        geldiğinde oyuncuya hediye veriyor.
+      </p>
+    </div>
+    <div class="col-lg-8 px-0">
+      <p class="lead">
+        Kardelen Okulu Kardebot gurubu karakteri.
+        <span class="border">Panda Rolly</span> ve Bambu macerası.
+      </p>
+    </div>
+
+    <a
+      href="https://gitlab.com/kaanguru/bambu"
+      target="_blank"
+      type="button"
+      class="btn btn-primary"
+      >Oyun Kodları</a
+    >
+
+    <img src="~/assets/img/map.png" class="img-fluid" alt="map" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
