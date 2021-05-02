fcb07821216288b6fc8cc1281110652d1e85c5ae
CEM KAAN KÖSALI <cemkaan@inbox.ru>
2021-05-02 12:55:25 +0300

    kullanım sözleşmesi

--- pages/index.vue
+++ pages/index.vue
@@ -58,6 +58,208 @@
     <div class="row">
       <img src="~/assets/img/map.png" class="img-fluid py-3" alt="map" />
     </div>
+    <div id="sartname" class="c5">
+      <h2 class="c6" id="h.4ptohme2n8n3">
+        <span class="c8"
+          >Kullan&#305;m &#350;artlar&#305; S&ouml;zle&#351;mesi</span
+        >
+      </h2>
+      <p class="c3"><span class="c1">Taraflar</span></p>
+      <p class="c3">
+        <span class="c1"
+          >&#304;&#351;bu s&ouml;zle&#351;me &ldquo;Kardelen Ortaokulu Robot
+          Tak&#305;m&#305; &nbsp;(bundan b&ouml;yle &ldquo;KARDEBOT&rdquo;
+          olarak an&#305;lacakt&#305;r) ile KARDEBOT(ya da kar&#351;&#305;
+          &#351;irketin ad&#305;) Mobil IOS uygulamalar&#305;ndan yararlanan
+          Kullan&#305;c&#305; ile kar&#351;&#305;l&#305;kl&#305; olarak kabul
+          edilerek y&uuml;r&uuml;rl&uuml;&#287;e girmi&#351;tir. Bu
+          s&ouml;zle&#351;me mevcutta KARDEBOT m&uuml;&#351;terisi olan
+          kurumlar&#305;n son kullan&#305;c&#305;lar&#305;n&#305; ilgilendirir
+          ve ana Lisans s&ouml;zle&#351;mesinin ekidir. Bu s&ouml;zle&#351;me
+          KARDEBOT Mobil Uygulamas&#305;n&#305; kullanacak
+          kullan&#305;c&#305;lar&#305;n Mobil Uygulamay&#305; cihazlar&#305;na
+          y&uuml;klemesi ile birlikte y&uuml;r&uuml;rl&uuml;&#287;e girmi&#351;
+          olacakt&#305;r. Kullan&#305;c&#305;lar&#305;n Mobil Uygulamadan
+          ayr&#305;lmak istemesi durumunda, uygulamay&#305; kald&#305;rmas&#305;
+          yeterli olacakt&#305;r.</span
+        >
+      </p>
+      <p class="c2"><span class="c0"></span></p>
+      <p class="c3">
+        <span class="c1">Cihaz Bilgisi&rsquo;ne Eri&#351;ilmesi</span>
+      </p>
+      <p class="c3">
+        <span class="c1"
+          >Kullan&#305;c&#305;lar Mobil Uygulamay&#305; y&uuml;kleyerek
+          giri&#351; yapt&#305;klar&#305;nda cihaz bilgileri (cihaz&#305;n
+          ekran&#305;n&#305;n a&ccedil;&#305;k olup olmama, GPS
+          ayar&#305;n&#305;n durumuna dair bilgiler), ki&#351;i bilgilerine
+          ula&#351;&#305;labilecektir.</span
+        >
+      </p>
+      <p class="c2"><span class="c0"></span></p>
+      <p class="c3">
+        <span class="c1">Bilgilerin Kullan&#305;lmas&#305;</span>
+      </p>
+      <p class="c3">
+        <span class="c1"
+          >KARDEBOT olarak Kullan&#305;c&#305;n&#305;n uygulama kullan&#305;m
+          s&uuml;releri boyunca hi&ccedil;bir ki&#351;isel verisi
+          taraf&#305;m&#305;za ait veri depolama alanlar&#305;nda
+          tutulmamaktad&#305;r. Kullan&#305;c&#305;ya ait uygulama
+          i&ccedil;erisindeki bilgiler (tercihler) Kullan&#305;c&#305;n&#305;n
+          tercihlerini s&#305;f&#305;rlamas&#305;, uygulamay&#305;
+          kald&#305;rmas&#305; gibi aksiyonlar ile cihaz i&ccedil;erisinden
+          silinebilir.</span
+        >
+      </p>
+      <p class="c2"><span class="c0"></span></p>
+      <p class="c3">
+        <span class="c1">Bilgilerin Payla&#351;&#305;lmas&#305;</span>
+      </p>
+      <p class="c3">
+        <span class="c1"
+          >KARDEBOT olarak Mobil Uygulama i&ccedil;erisindeki
+          Kullan&#305;c&#305;ya ait bilgilerin hi&ccedil;bir &#351;ekilde
+          taraf&#305;m&#305;za ait veri depolama alanlar&#305;nda
+          tutulmad&#305;&#287;&#305; a&ccedil;&#305;klanm&#305;&#351;t&#305;r.
+          &#304;&#351;bu s&ouml;zle&#351;meye dayanarak kay&#305;t alt&#305;nda
+          olmayan veriler &uuml;&ccedil;&uuml;nc&uuml; taraflar ile
+          payla&#351;&#305;lmayacakt&#305;r.</span
+        >
+      </p>
+      <p class="c2"><span class="c0"></span></p>
+      <p class="c3">
+        <span class="c1">Kullan&#305;c&#305; &#304;zinleri</span>
+      </p>
+      <p class="c3">
+        <span class="c1"
+          >Uygulaman&#305;n &ouml;zelliklerinin cihaz ile uyumlu
+          &ccedil;al&#305;&#351;abilmesi i&ccedil;in Kullan&#305;c&#305;
+          taraf&#305;ndan onaylanmas&#305; gereken ve onaylanmadan
+          al&#305;nm&#305;&#351; izinlerdir.</span
+        >
+      </p>
+      <p class="c2"><span class="c0"></span></p>
+      <p class="c3"><span class="c1">Konum &#304;zni</span></p>
+      <p class="c3">
+        <span class="c1"
+          >Bu izin Kullan&#305;c&#305;n&#305;n konumunu uygulama taraf&#305;ndan
+          y&ouml;nlendirilerek kullan&#305;m&#305;n&#305; i&ccedil;erir.
+          Kullan&#305;m esnas&#305;nda uygulama y&ouml;nlendirmesi ile
+          kullan&#305;c&#305; kararlar&#305; do&#287;rultusunda i&#351;lem
+          yap&#305;l&#305;r.
+        </span>
+      </p>
+      <p class="c2"><span class="c0"></span></p>
+      <p class="c3"><span class="c1">&#304;nternet &#304;zni</span></p>
+      <p class="c3">
+        <span class="c1"
+          >Bu izin uygulaman&#305;n &ccedil;al&#305;&#351;mas&#305;
+          esnas&#305;nda kurumun merkezi sunucusu &uuml;zerinde bulunan
+          servislere eri&#351;imi i&ccedil;in kullan&#305;l&#305;r. Bu
+          eri&#351;im esnas&#305;nda kullan&#305;lacak olan internet
+          ba&#287;lant&#305;s&#305; kullan&#305;c&#305;
+          sorumlulu&#287;undad&#305;r.</span
+        >
+      </p>
+      <p class="c2"><span class="c0"></span></p>
+      <p class="c3"><span class="c1">Depolama &#304;zni</span></p>
+      <p class="c3">
+        <span class="c1"
+          >Bu izin cihaz&#305;n belle&#287;inde uygulama i&ccedil;erisinde
+          kullan&#305;lan ikonlar&#305;n ve uygulamaya ait veri
+          taban&#305;n&#305;n saklanmas&#305;na olanak verir.</span
+        >
+      </p>
+      <p class="c2"><span class="c0"></span></p>
+      <p class="c3">
+        <span class="c1">Fikri M&uuml;lkiyet Haklar&#305;</span>
+      </p>
+      <p class="c3">
+        <span class="c1"
+          >Uygulaman&#305;n (genel g&ouml;r&uuml;n&uuml;m, tasar&#305;m, metin,
+          imge, logo, ikon, yaz&#305;l&#305;, elektronik, grafik veya makinede
+          okunabilir &#351;ekilde sunulan teknik verileri, KARDEBOT
+          markas&#305;, uygulanan i&#351; metodu ve i&#351; modeli,
+          yaz&#305;l&#305;m kodu ve di&#287;er kodlar da dahil ve fakat bunlarla
+          s&#305;n&#305;rl&#305; olmamak kayd&#305;yla) t&uuml;m unsurlar&#305;
+          KARDEBOT&rsquo;a aittir ve/veya KARDEBOT taraf&#305;ndan
+          &uuml;&ccedil;&uuml;nc&uuml; bir ki&#351;iden al&#305;nan telif
+          hakk&#305; alt&#305;nda kullan&#305;lmaktad&#305;r.</span
+        >
+      </p>
+      <p class="c2"><span class="c0"></span></p>
+      <p class="c3">
+        <span class="c1">Hizmet ve Gizlilik Ko&#351;ullar&#305;</span>
+      </p>
+      <p class="c3">
+        <span class="c1"
+          >KARDEBOT, ki&#351;isel bilgi ve veri g&uuml;venli&#287;ini
+          &ouml;nemsemekte ve bu hususta gerekli olan t&uuml;m tedbirleri almaya
+          &ouml;zen g&ouml;stermektedir. Kullan&#305;c&#305;lar da
+          Uygulamay&#305; kullanarak i&#351;bu gizlilik s&ouml;zle&#351;mesi
+          h&uuml;k&uuml;mlerine uygun davranacaklar&#305;n&#305; kabul, beyan ve
+          taahh&uuml;t ederler. &#304;&#351;bu Gizlilik S&ouml;zle&#351;mesi,
+          Uygulaman&#305;n t&uuml;m b&ouml;l&uuml;mlerinde ge&ccedil;erli
+          olacakt&#305;r. Kullan&#305;c&#305;lar&#305;n bilgilerinin
+          korunmas&#305; ve gizlili&#287;in s&uuml;rd&uuml;r&uuml;lebilmesi
+          KARDEBOT&rsquo;un birinci &ouml;nceli&#287;idir. Bu nedenle
+          Kullan&#305;c&#305;lar&#305;n vermi&#351; oldu&#287;u bilgiler
+          s&ouml;zle&#351;mede belirtilen kurallar ve ama&ccedil;lar
+          d&#305;&#351;&#305;nda herhangi bir kapsamda kullan&#305;lmayacak,
+          &uuml;&ccedil;&uuml;nc&uuml; &#351;ah&#305;slarla
+          payla&#351;&#305;lmayacakt&#305;r. Uygulamadaki sistemle ilgili
+          sorunlar&#305;n tan&#305;mlanabilmesi ve Uygulamada
+          &ccedil;&#305;kabilecek muhtemel sorunlar&#305;n acil olarak
+          giderilmesi i&ccedil;in, KARDEBOT gerekti&#287;inde
+          Kullan&#305;c&#305;lar&#305;n IP adresini, cihaz &uuml;zerindeki
+          tarih&ccedil;e bilgilerini kaydedebilir ve bu kay&#305;tlar&#305;
+          an&#305;lan bu ama&ccedil;larla kullanabilir. Uygulama kapsam&#305;nda
+          elde edilen bilgiler KARDEBOT taraf&#305;ndan
+          Kullan&#305;c&#305;n&#305;n kimli&#287;i hi&ccedil; bir &#351;ekilde
+          a&ccedil;&#305;&#287;a &ccedil;&#305;kar&#305;lmaks&#305;z&#305;n
+          sadece &ccedil;e&#351;itli istatistiki de&#287;erlendirmeler dahilinde
+          kullan&#305;labilir. KARDEBOT, a&#351;a&#287;&#305;da say&#305;lan
+          hallerde ise i&#351;bu gizlilik bildirimi h&uuml;k&uuml;mleri
+          d&#305;&#351;&#305;na &ccedil;&#305;karak kullan&#305;c&#305;lara ait
+          bilgileri &uuml;&ccedil;&uuml;nc&uuml; ki&#351;ilere
+          a&ccedil;&#305;klayabilecektir. Bu durumlar; Hukuk
+          kurallar&#305;n&#305;n getirdi&#287;i zorunluluklara
+          uyulmas&#305;n&#305;n gerekti&#287;i haller, KARDEBOT&rsquo;un,
+          Kullan&#305;c&#305;lar&#305;yla aras&#305;ndaki
+          s&ouml;zle&#351;melerin gereklerinin yerine getirilmesi ve
+          bunlar&#305;n uygulamaya konulmalar&#305;yla ilgili hallerde, Yetkili
+          idari ve/veya adli makamlar taraf&#305;ndan usuli y&ouml;ntemine uygun
+          olarak y&uuml;r&uuml;t&uuml;len bir ara&#351;t&#305;rma veya
+          soru&#351;turma do&#287;rultusunda Kullan&#305;c&#305;larla ilgili
+          bilgi talep edilmesi halleri, Kullan&#305;c&#305;lar&#305;n
+          haklar&#305;n&#305; veya g&uuml;venliklerini koruma amac&#305;yla
+          bilgi verilmesinin gerekli oldu&#287;u haller. KARDEBOT, kendisine
+          verilen gizli bilgileri kesinlikle &ouml;zel ve gizli tutmay&#305;,
+          bunu bir s&#305;r olarak saklamay&#305; y&uuml;k&uuml;ml&uuml;l&uuml;k
+          olarak kabul etti&#287;ini ve gizlili&#287;in sa&#287;lan&#305;p
+          s&uuml;rd&uuml;r&uuml;lmesi, gizli bilginin tamam&#305;n&#305;n veya
+          herhangi bir par&ccedil;as&#305;n&#305;n kamu alan&#305;na girmesini
+          veya yetkisiz kullan&#305;m&#305;n&#305; veya
+          &uuml;&ccedil;&uuml;nc&uuml; bir ki&#351;iye
+          a&ccedil;&#305;klanmas&#305;n&#305; &ouml;nleme gere&#287;i olan
+          gerekli t&uuml;m tedbirleri almay&#305; ve &uuml;zerine
+          d&uuml;&#351;en t&uuml;m &ouml;zeni tam olarak g&ouml;stermeyi
+          taahh&uuml;t etmektedir. KARDEBOT, i&#351;bu gizlilik bildiriminde
+          ge&ccedil;en h&uuml;k&uuml;mleri gerekli g&ouml;rd&uuml;&#287;&uuml;
+          zamanda Uygulamada yay&#305;nlamak &#351;art&#305;yla
+          de&#287;i&#351;tirebilir. KARDEBOT&rsquo;un de&#287;i&#351;iklik
+          yapt&#305;&#287;&#305; gizlilik bildirimi h&uuml;k&uuml;mleri
+          Uygulamada yay&#305;nland&#305;&#287;&#305; tarihte
+          y&uuml;r&uuml;rl&uuml;&#287;e girmi&#351; kabul edilir. &#304;&#351;bu
+          Gizlilik S&ouml;zle&#351;mesi, 9 (dokuz) maddeden ibaret olup Mobil
+          Uygulama Kullan&#305;c&#305; S&ouml;zle&#351;mesinin ayr&#305;lmaz bir
+          par&ccedil;as&#305;d&#305;r.</span
+        >
+      </p>
+      <p class="c7"><span class="c4"></span></p>
+    </div>
   </div>
 </template>
 
@@ -66,4 +268,216 @@ export default {};
 </script>
 
 <style>
+ol {
+  margin: 0;
+  padding: 0;
+}
+table td,
+table th {
+  padding: 0;
+}
+.c6 {
+  border-right-style: solid;
+  padding-top: 0pt;
+  border-top-width: 0pt;
+  border-right-width: 0pt;
+  padding-left: 0pt;
+  padding-bottom: 0pt;
+  line-height: 1.25;
+  border-left-width: 0pt;
+  border-top-style: solid;
+  margin-right: 22pt;
+  margin-left: 22pt;
+  border-left-style: solid;
+  border-bottom-width: 0pt;
+  border-bottom-style: solid;
+  orphans: 2;
+  widows: 2;
+  text-align: center;
+  padding-right: 0pt;
+}
+.c3 {
+  border-right-style: solid;
+  padding-top: 0pt;
+  border-top-width: 0pt;
+  border-right-width: 0pt;
+  padding-left: 0pt;
+  padding-bottom: 0pt;
+  line-height: 1.15;
+  border-left-width: 0pt;
+  border-top-style: solid;
+  border-left-style: solid;
+  border-bottom-width: 0pt;
+  border-bottom-style: solid;
+  orphans: 2;
+  widows: 2;
+  text-align: left;
+  padding-right: 0pt;
+}
+.c4 {
+  color: #000000;
+  font-weight: 400;
+  text-decoration: none;
+  vertical-align: baseline;
+  font-size: 11pt;
+  font-family: "Arial";
+  font-style: normal;
+}
+.c0 {
+  color: #000000;
+  font-weight: 400;
+  text-decoration: none;
+  vertical-align: baseline;
+  font-size: 16pt;
+  font-family: "Arial";
+  font-style: normal;
+}
+.c7 {
+  padding-top: 0pt;
+  padding-bottom: 0pt;
+  line-height: 1.15;
+  orphans: 2;
+  widows: 2;
+  text-align: left;
+  height: 11pt;
+}
+.c8 {
+  color: #000000;
+  font-weight: 700;
+  text-decoration: none;
+  vertical-align: baseline;
+  font-size: 38.5pt;
+  font-family: "Arial";
+  font-style: normal;
+}
+.c1 {
+  color: #000000;
+  font-weight: 400;
+  text-decoration: none;
+  vertical-align: baseline;
+  font-size: 13.5pt;
+  font-family: "Arial";
+  font-style: italic;
+}
+.c2 {
+  padding-top: 0pt;
+  padding-bottom: 0pt;
+  line-height: 1.15;
+  orphans: 2;
+  widows: 2;
+  text-align: center;
+  height: 11pt;
+}
+.c5 {
+  background-color: #ffffff;
+  padding: 30pt;
+}
+.title {
+  padding-top: 0pt;
+  color: #000000;
+  font-size: 26pt;
+  padding-bottom: 3pt;
+  font-family: "Arial";
+  line-height: 1.15;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left;
+}
+.subtitle {
+  padding-top: 0pt;
+  color: #666666;
+  font-size: 15pt;
+  padding-bottom: 16pt;
+  font-family: "Arial";
+  line-height: 1.15;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left;
+}
+li {
+  color: #000000;
+  font-size: 11pt;
+  font-family: "Arial";
+}
+p {
+  margin: 0;
+  color: #000000;
+  font-size: 11pt;
+  font-family: "Arial";
+}
+h1 {
+  padding-top: 20pt;
+  color: #000000;
+  font-size: 20pt;
+  padding-bottom: 6pt;
+  font-family: "Arial";
+  line-height: 1.15;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left;
+}
+h2 {
+  padding-top: 18pt;
+  color: #000000;
+  font-size: 16pt;
+  padding-bottom: 6pt;
+  font-family: "Arial";
+  line-height: 1.15;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left;
+}
+h3 {
+  padding-top: 16pt;
+  color: #434343;
+  font-size: 14pt;
+  padding-bottom: 4pt;
+  font-family: "Arial";
+  line-height: 1.15;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left;
+}
+h4 {
+  padding-top: 14pt;
+  color: #666666;
+  font-size: 12pt;
+  padding-bottom: 4pt;
+  font-family: "Arial";
+  line-height: 1.15;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left;
+}
+h5 {
+  padding-top: 12pt;
+  color: #666666;
+  font-size: 11pt;
+  padding-bottom: 4pt;
+  font-family: "Arial";
+  line-height: 1.15;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left;
+}
+h6 {
+  padding-top: 12pt;
+  color: #666666;
+  font-size: 11pt;
+  padding-bottom: 4pt;
+  font-family: "Arial";
+  line-height: 1.15;
+  page-break-after: avoid;
+  font-style: italic;
+  orphans: 2;
+  widows: 2;
+  text-align: left;
+}
 </style>
